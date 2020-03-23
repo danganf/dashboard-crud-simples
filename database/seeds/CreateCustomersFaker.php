3db5ef1ae5846cf6fdaedf85a753bf680228e14c
danielazeveedo2k12 <danielazeveedo2k12@gmail.com>
2020-03-23 11:17:09 -0300

    faker catalog e fixed faker customer

--- database/seeds/CreateCustomersFaker.php
+++ database/seeds/CreateCustomersFaker.php
@@ -9,20 +9,23 @@ class CreateCustomersFaker extends Seeder
      *
      * @return void
      */
-    public function run( \Danganf\MyClass\JsonBasic $json )
+    public function run()
     {
         $faker     = Faker\Factory::create();
         $fakerBR   = Faker\Factory::create('pt_BR');
-        $faker->addProvider(new \JansenFelipe\FakerBR\FakerBR($faker));
+        $faker->addProvider(new JansenFelipe\FakerBR\FakerBR($faker));
 
         for ( $i=1; $i<=50; $i++ ){
-            $data['name']     = $faker->name;
-            $data['email']    = $faker->email;
+            $data['name']     = $fakerBR->name;
+            $data['email']    = $fakerBR->email;
             $data['document'] = $faker->cpf;
-            $data['phone']    = substr( $faker->e164PhoneNumber, 3 );
+            $data['phone']    = '21' . str_replace( '-', '', $fakerBR->cellphone );
             $data['status']   = 1;
+
+            $json = \App::make('App\MyClass\Json\CustomerSave');
             $json->set( json_encode( $data ) );
-            ( new \App\Repositories\CustomerRepository() )->createOrUpdate( $json );
+
+            ( new App\Repositories\CustomerRepository() )->createOrUpdate( $json );
         }
     }
 }
