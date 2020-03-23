2701cd5ab0597ae8a871ce516a9320da9e407a02
danielazeveedo2k12 <danielazeveedo2k12@gmail.com>
2020-03-23 17:02:45 -0300

    catalog.index

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\MyClass\FactoryApis;
+use App\MyClass\Traits\OpenViewController;
 use Illuminate\Http\Request;
 
 class CatalogController extends Controller
 {
-    //
+    use OpenViewController;
+
+    private $title    = 'Catalogo';
+    private $pathView = 'catalog';
+    private $subtitle;
+
+    public function index( Request $request, FactoryApis $factoryApis ){
+
+        $this->subtitle = 'Catalogo';
+
+        $filtersTmp           = getVariablesFilter($request);
+        $filtersTmp['limit']  = ( array_get( $filtersTmp, 'limit', 10) );
+        $filtersTmp['order']  = 'name';
+
+        $factoryApis->setFilters( $filtersTmp );
+        $result = $factoryApis->get('catalog');
+
+        return $this->openView([
+            'results'        => array_pull( $result, 'data' ),
+            'filters'        => $filtersTmp,
+            'paginator'      => $result,
+        ]);
+    }
 }
---
+++ resources/views/pages/catalog/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="row">
+        {{--@include($basicPath.'.sections.filter')--}}
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header reload-card" id="last-orders">
+                    <h4 class="card-title"></h4>
+                    <div class="heading-elements">
+                        <button onclick="location.href='{{route('catalog.new')}}'" class="btn btn-primary btn-sm btn-icon"><i class="ft-plus white"></i> Novo</button>
+                        <span class="dropdown"></span>
+                    </div>
+                </div>
+                <div class="card-content collapse show">
+                    <div id="last-orders" class="media-list position-relative">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-xl mb-0">
+                                <thead>
+                                <tr>
+                                    <th class="border-top-0" style="width: 5%">#</th>
+                                    <th class="border-top-0">Nome</th>
+                                    <th class="border-top-0 text-right">Ação</th>
+                                </tr>
+                                </thead>
+                                <tbody class="value-last-orders">
+                                @foreach( $results AS $row )
+                                    <tr>
+                                        <td class="text-truncate">{{$row['id']}}</td>
+                                        <td class="text-truncate">
+                                            <a href="{{route('catalog.edit',[$row['id']])}}">
+                                                {{$row['name']}}
+                                            </a>
+                                        </td>
+                                        <td class="text-truncate text-right">
+                                            <a class="btn-delete" data-id="{{$row['id']}}"><i class="la la-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    @component('components.paginator', [ 'paginator' => $paginator ] )@endcomponent
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('js')
+
+    <script>
+        cruds.bindDelete('customer');
+        @include('includes.filter-js')
+    </script>
+@endsection
